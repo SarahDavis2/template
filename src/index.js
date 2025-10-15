875c34a64199dd6bcae1ece19c7f6183afd1ec32
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-10-14 19:23:33 -0500

    Feat: webpack dev & prod files

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@
 import "./styles/styles.css";
 import { greeting } from "./scripts/greeting.js";
 
+if (process.env.NODE_ENV !== 'production') {
+   console.log('Looks like we are in development mode!');
+}
+
 console.log(greeting);
+
