d66a6497000c4b84759c4d52ee172f9120b71745
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-11-03 10:42:31 -0600

    Feat: format files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import "./styles/reset.css";
 import "./styles/styles.css";
 import { greeting } from "./scripts/greeting.js";
 
-if (process.env.NODE_ENV !== 'production') {
-   console.log('Looks like we are in development mode!');
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
 }
 
 console.log(greeting);
